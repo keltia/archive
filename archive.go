74d6fa7c051b144edc5394829979c42c7091ec25
Ollivier Robert <roberto@keltia.net>
2018-11-05 17:03:57 +0100

    gofmt pass.

--- archive.go
+++ archive.go
@@ -32,13 +32,13 @@ func SetVerbose() {
 
 // SetDebug sets the mode too
 func SetDebug() {
-	fDebug   = true
+	fDebug = true
 	fVerbose = true
 }
 
 // Reset is for the two flags
 func Reset() {
-	fDebug   = false
+	fDebug = false
 	fVerbose = false
 }
 
